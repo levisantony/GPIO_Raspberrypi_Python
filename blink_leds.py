908768980235bea0ffe6baa04b414f9e9d7ba2bd
Levis Antony <levisantony@gmail.com>
2013-04-28 12:48:34 +0530

    Update blink_leds.py

--- blink_leds.py
+++ blink_leds.py
@@ -4,7 +4,7 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 # Setting pins(11,12,13,15,16,18) as output
-print "Setup Pin 11,12,13,15,16,18"
+print("Setup Pin 11,12,13,15,16,18")
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
@@ -13,9 +13,9 @@ GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
 var=1
-print "Start Blinking"
+print("Start Blinking")
 while var==1 :
-  print "Leds OFF"
+  print("Leds OFF")
   GPIO.output(11, False)
   GPIO.output(12, False)
   GPIO.output(13, False)
@@ -23,7 +23,7 @@ while var==1 :
   GPIO.output(16, False)
   GPIO.output(18, False)
   time.sleep(1)
-  print "Leds On"
+  print("Leds On")
   GPIO.output(11, True)
   GPIO.output(12, True)
   GPIO.output(13, True)
